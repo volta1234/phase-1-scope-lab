6e516f1bd012409aa5c86927a6e543e861ab320a
CALVINCE <calvince.ogwel@gmail.com>
2022-11-26 18:08:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = "BOB";
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "X";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Z";
+}
